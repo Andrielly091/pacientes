6810cd8f5c587ae26a6d6fe33bae6cb21baa9c59
Andrielly091 <afl5@discente.ifpe.edu.br>
2024-08-18 00:11:58 -0300

    Enviando projeto

--- src/br/edu/ifpe/apae/main/Main.java
+++ src/br/edu/ifpe/apae/main/Main.java
@@ -4,6 +4,7 @@ import br.edu.ifpe.apae.excecoes.ExcecaoNegocio;
 
 public class Main {
 	public static void main(String[] args) throws ExcecaoNegocio {
-		new TelaPaciente().exibir();
+		new TelaPaciente().exibir();  
+		
 	}
 }
